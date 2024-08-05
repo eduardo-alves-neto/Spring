310e834a33c00e734a4632ae917e2566beebc6ea
eduardo-alves-neto <emailforprojects01@gmail.com>
2024-08-05 15:39:16 -0300

    init project

---
+++ backEnd/src/main/java/com/ProjectSpring/Delivire/DelivireApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectSpring.Delivire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelivireApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/ProjectSpring/Delivire/DelivireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectSpring.Delivire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelivireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
